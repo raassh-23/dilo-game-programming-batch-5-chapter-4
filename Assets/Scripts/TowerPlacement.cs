5dbd84fc9c65ad0dfe584a4506a38831a98853bf
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-11-12 22:04:18 +0700

    Add project

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    private Tower _placedTower;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(_placedTower != null) {
+            return;
+        }
+
+        Tower tower = other.GetComponent<Tower>();
+        if (tower != null) {
+            tower.SetPlacePosition(transform.position);
+            _placedTower = tower;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (_placedTower == null) {
+            return;
+        }
+
+        _placedTower.SetPlacePosition(null);
+        _placedTower = null;
+    }
+}
